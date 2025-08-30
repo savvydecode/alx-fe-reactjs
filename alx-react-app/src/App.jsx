c6b6e5b4b0669cb8af6f0cd0d9439ce42e63b209
savvycode <addostephen557@gmail.com>
2025-08-30 19:25:33 +0000

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,5 @@
 
-import WelcomeMessage from "./components/WelcomeMessage";
+import WelcomeMessage from "./components/WelcomeMessage.jsx";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
