e72af760566c7fa1e3f5f15973d5c22dcb2f899e
savvycode <addostephen557@gmail.com>
2025-08-30 19:41:44 +0000

    update

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
-const UserProfile = (props) => {
+const UserProfile = ({name, age, bio}) => {
     return (
         <>
-            <h2>{props.name}</h2>
-            <p>{props.age}</p>
-            <p>{props.bio}</p>
+            <h2>{name}</h2>
+            <p>{age}</p>
+            <p>{bio}</p>
         </>
     )
 };
