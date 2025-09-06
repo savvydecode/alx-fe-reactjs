77e65b6bb2bb2f3f1311043995917f2053c55a61
savvycode <addostephen557@gmail.com>
2025-09-06 22:15:32 +0000

    updata

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -5,8 +5,8 @@ const UserProfile = (props) => {
             padding: '10px',
             margin: '10px'
         }}>
-            <h2 style={{  color: 'blue' }} >{props.name}</h2>
-            <p style={{fontWeight: 'bold'}} >Age: {props.age}</p>
+            <h2 style={{ color: 'blue' }} >{props.name}</h2>
+            <p>Age: <span style={{ fontWeight: 'bold' }} >{props.age}</span></p>
             <p>Bio: {props.bio}</p>
         </div>
     )
