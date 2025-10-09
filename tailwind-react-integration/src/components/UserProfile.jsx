8554873f2167b38dc3a23f76fa758e2aea7c1387
savvycode <addostephen557@gmail.com>
2025-10-09 03:37:46 +0000

    implementing responsive design

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
     return (
-        <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+        <div className="user-profile bg-gray-100 max-w-sm sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg p-8 sm:p-4 md:p-8">
             <img  
-            className="rounded-full w-36 h-36 mx-auto"
+            className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
             src="https://placehold.co/150" alt="User" />
             
             <h1
-            className="text-xl text-blue-800 my-4"
+            className="text-xl text-blue-800 my-4 sm:text-sm md:text-base"
             >John Doe</h1>
             <p
             className="text-gray-600 text-base"
