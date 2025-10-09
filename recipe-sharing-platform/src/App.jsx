004750696e0e88d1e02c32c105c4c32330fc1fa8
savvycode <addostephen557@gmail.com>
2025-10-09 14:20:37 +0000

    configure tailwindcss for recipe-sharing-platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-blue-500'>hi</p>
+    </>
+  )
+}
+
+export default App
---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: "media", // You can set it to 'media' or 'class' if needed
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
